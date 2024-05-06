b915125ae0698ff7acf9bfc9ff23a9bd64221031
toto rototo <el.totorototo@gmail.com>
2024-05-06 23:57:41 +0200

    fix: test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,8 +23,8 @@ const setFonts = () => {
 
 const GlobalStyle = createGlobalStyle`
   html{
-    min-height: calc(100% + env(safe-area-inset-top));
-    padding: env(safe-area-inset-top) env(safe-area-inset-right) env(safe-area-inset-bottom) env(safe-area-inset-left);
+    // min-height: calc(100% + env(safe-area-inset-top));
+    // padding: env(safe-area-inset-top) env(safe-area-inset-right) env(safe-area-inset-bottom) env(safe-area-inset-left);
   }
   body {
     height:100vh;
